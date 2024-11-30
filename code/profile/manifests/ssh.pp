dfde7e391e1f66db42da6e2620c265cd0edc18f5
Stéphane Gerard <stgerard@vub.ac.be>
2024-11-30 15:08:47 +0100

    doc

--- code/profile/manifests/ssh.pp
+++ code/profile/manifests/ssh.pp
@@ -1,4 +1,16 @@
 # Manage sshd config
+# Document class parameters
+# @param [Array[String]] $allow_users List of users to allow
+# @example
+#   class { 'profile::ssh':
+#     allow_users => ['root', 'vagrant'],
+#   }
+# @note
+#   This class will install the openssh-server package, configure the sshd_config file, and start the sshd service.
+#   The allow_users parameter is an array of users to allow.
+#   The default value is ['root'].
+#   The sshd_config file is a template that allows only the users in the allow_users array.
+#   The sshd service is restarted when the sshd_config file changes.
 class profile::ssh (
   Array[String] $allow_users = ['root'],
 ) {
