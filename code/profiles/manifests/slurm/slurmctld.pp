2283a8155a34c16504c47315be17e9bf677f478b
Stéphane Gerard <stgerard@vub.ac.be>
2024-12-04 18:48:29 +0100

    in EL9, s-nail replace mailx package.

--- code/profiles/manifests/slurm/slurmctld.pp
+++ code/profiles/manifests/slurm/slurmctld.pp
@@ -1,7 +1,10 @@
 # Profile class used for setting up a Slurm Head node (where the slurmctld daemon runs)
-class profiles::slurm::slurmctld {
-  package { 's-nail':
-    ensure => present,
+class profiles::sluirm::slurmctld {
+  # In EL9, the s-nail package replace mailx
+  if ( $facts['os']['family'] == 'RedHat' and $facts['os']['release']['major'] == '9' ) {
+    package { 's-nail':
+      ensure => present,
+    }
   }
   include slurm::slurmctld
 }
