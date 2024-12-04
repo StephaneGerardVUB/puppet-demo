db1607d4602de2a94d85609a05a560f2f6e85c32
Stéphane Gerard <stgerard@vub.ac.be>
2024-12-04 18:49:31 +0100

    typo

--- code/profiles/manifests/slurm/slurmctld.pp
+++ code/profiles/manifests/slurm/slurmctld.pp
@@ -1,5 +1,5 @@
 # Profile class used for setting up a Slurm Head node (where the slurmctld daemon runs)
-class profiles::sluirm::slurmctld {
+class profiles::slurm::slurmctld {
   # In EL9, the s-nail package replace mailx
   if ( $facts['os']['family'] == 'RedHat' and $facts['os']['release']['major'] == '9' ) {
     package { 's-nail':
