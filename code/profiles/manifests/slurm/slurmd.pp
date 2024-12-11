62e4f252fbeb6821c72199e337a72d8642ef5a0f
Stéphane Gerard <stgerard@vub.ac.be>
2024-12-11 17:20:47 +0100

    another attempt to add a qos

--- code/profiles/manifests/slurm/slurmd.pp
+++ code/profiles/manifests/slurm/slurmd.pp
@@ -3,5 +3,14 @@ class profiles::slurm::slurmd {
   package { 'dbus-devel':
     ensure => installed,
   }
+  slurm::acct::qos { 'qos-interactive':
+    ensure   => 'present',
+    priority => 20,
+    options  => {
+      preempt  => 'qos-besteffort',
+      grpnodes => 30,
+    },
+  }
+
   include slurm::slurmd
 }
